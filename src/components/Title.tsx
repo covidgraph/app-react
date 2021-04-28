d864fce1388db28597ac0f5032966e84f4f23197
Dav Singh <davinder.cheema@prodyna.com>
2021-04-28 09:19:47 +0100

    Updated react-scripts, remove propTypes not needed with TS

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 
-export default function Title(props: any) {
+type TitleProps = {
+  children: React.ReactNode
+}
+
+const Title:React.FunctionComponent<TitleProps> = (props) => {
   return (
     <Typography component="h2" variant="h6" color="primary" gutterBottom>
       {props.children}
@@ -10,6 +13,4 @@ export default function Title(props: any) {
   )
 }
 
-Title.propTypes = {
-  children: PropTypes.node,
-}
+export default Title;
